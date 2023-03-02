5c6dbe555d51d266c6a96c737abe53329c0b840a
Lautaro Mielgo <lautimielgo@gmail.com>
2023-03-01 22:13:56 -0300

    prueba henry challenge

--- 01.js
+++ 01.js
@@ -11,6 +11,15 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var arrayDeNumeros = [];
+
+  for(i = 0 ; i < array.length; i++){
+    if(typeof array[i] === "number"){
+      arrayDeNumeros.push(array[i])
+    }
+  }
+  return arrayDeNumeros
+
 
 }
 
